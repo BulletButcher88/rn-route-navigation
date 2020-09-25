6c4910284224f4fb50a9c69915c6b31fe8a4cd16
Mark Butcher <markjamesbutcher@gmail.com>
2020-09-26 07:46:37 +1000

    playing around

--- components/RenderGridItem.js
+++ components/RenderGridItem.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { TouchableOpacity, Text, View, StyleSheet, Image } from 'react-native'
+import { TouchableOpacity, Text, View, StyleSheet, Image, Platform } from 'react-native'
+import { TouchableNativeFeedback } from 'react-native-gesture-handler';
 import Colors from '../constants/Colors'
 
 const RenderGridItem = props => {
+  let TouchableCmp = TouchableOpacity
+
+  if (Platform.OS == 'android' && Platform.Version >= 21) {
+    TouchableCmp = TouchableNativeFeedback
+  }
   return (
     <View>
-      <TouchableOpacity
+      <TouchableCmp
         style={{ ...styles.girdItems, backgroundColor: props.color }}
         onPress={props.onSelect}>
         {props.image ? <Image
@@ -13,10 +19,10 @@ const RenderGridItem = props => {
 
           source={{ uri: props.image }}
         /> : null}
-      </TouchableOpacity >
+      </TouchableCmp >
 
       <View style={styles.textContainer}>
-        <Text style={styles.titleText}>{props.title}</Text>
+        <Text style={styles.titleText} numberOfLines={2}>{props.title}</Text>
         <Text style={styles.descriptionText}>{props.description}</Text>
       </View>
     </View>
@@ -26,7 +32,7 @@ const RenderGridItem = props => {
 const styles = StyleSheet.create({
   girdItems: {
     flex: 1,
-    marginTop: 10,
+    margin: 5,
     marginHorizontal: 10,
     height: 180,
     width: 300,
@@ -46,17 +52,18 @@ const styles = StyleSheet.create({
     marginLeft: 18,
     height: 80,
     maxWidth: 280,
-
   },
   titleText: {
+    marginHorizontal: 12,
     marginTop: 5,
     fontSize: 20,
     color: 'black',
     fontFamily: 'roboto',
-    color: Colors.fadedTextGrey
+    textAlign: 'right'
   },
   descriptionText: {
     marginVertical: 5,
+    marginHorizontal: 12,
     color: Colors.fadedTextGrey
   },
   photoStyle: {
--- constants/Colors.js
+++ constants/Colors.js
@@ -1,5 +1,6 @@
 export default {
   primaryColor: '#ffbfbf',
   accentColor: '#FFCF55',
-  fadedTextGrey: '#8D8C8C'
+  strongTextGrey: '#8C8C8C',
+  fadedTextGrey: '#8D8C8C',
 }
